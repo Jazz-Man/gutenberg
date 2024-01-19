a8a418a183071ff45e7d95b7fb43039c2df00824
Dave Smith <getdavemail@gmail.com>
2024-01-19 15:35:48 +0000

    Activating a link via keyboard or toolbar goes to preview mode

--- packages/format-library/src/link/inline.js
+++ packages/format-library/src/link/inline.js
@@ -264,7 +264,6 @@ function InlineLinkUI( {
 				value={ linkValue }
 				onChange={ onChangeLink }
 				onRemove={ removeLink }
-				forceIsEditingLink={ addingLink }
 				hasRichPreviews
 				createSuggestion={ createPageEntity && handleCreate }
 				withCreateSuggestion={ userCanCreatePages }
