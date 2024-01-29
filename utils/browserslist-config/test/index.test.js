eba00e088feebf6536b89b4000a4000eab367fd3
Vasyl Sokolyk <vsokolyk@gmail.com>
2024-01-29 11:59:59 +0200

    build(pnpm): rearange workspace

--- utils/browserslist-config/test/index.test.js
+++ utils/browserslist-config/test/index.test.js
@@ -6,7 +6,7 @@ import browserslist from 'browserslist';
 /**
  * Internal dependencies
  */
-import config from '../';
+import config from '../index';
 
 it( 'should export an array', () => {
 	expect( Array.isArray( config ) ).toBe( true );
